a6cddd731329186814da79e67db1181b7bed2d66
USER <kaushalyaamandi080@gmail.com>
2021-06-01 11:50:52 +0530

    Completed movie list screen

--- lib/interfaces/Movie_List.dart
+++ lib/interfaces/Movie_List.dart
@@ -59,7 +59,6 @@ class _MovieListScreenState extends State<MovieListScreen>{
                       searchBarStyle: SearchBarStyle(
                         borderRadius: BorderRadius.circular(56),
                         backgroundColor: Colors.white,
-                        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
                       ),
                     ),
                   )
@@ -101,11 +100,13 @@ class _MovieListScreenState extends State<MovieListScreen>{
                             borderRadius: BorderRadius.all(Radius.circular(8.0)),
                             color: Colors.black,
                           ),
-                          child: Image.asset(
-                            'images/Avengers.jpg',
-                            height: 150.0,
-                            width: 100.0,
-                          ),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(25),
+                              child: Image.asset(
+                                'images/Avengers.jpg',
+                                fit: BoxFit.fitWidth,
+                                width: double.infinity,
+                              ))
                         ),
                       ],
                     ),
@@ -125,25 +126,148 @@ class _MovieListScreenState extends State<MovieListScreen>{
                           ],
                         ),
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
                             Text(
                               'Movie Description',
                               style: TextStyle(
                                 fontSize: 15,
-                                color: Colors.white,
+                                color: Colors.grey,
                               ),
                             )
                           ],
                         ),
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            ClipRect(
-
+                            SizedBox(
+                              width: 60.0,
+                              height: 20.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.shade800,
+                                    boxShadow: [
+                                      BoxShadow(color: Colors.black12, offset: Offset(3.0, 6.0), blurRadius: 10.0),
+                                    ],
+                                  ),
+                                  child: Text(
+                                    'year',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            SizedBox(
+                              width: 60.0,
+                              height: 20.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.shade800,
+                                    boxShadow: [
+                                      BoxShadow(color: Colors.black12, offset: Offset(3.0, 6.0), blurRadius: 10.0),
+                                    ],
+                                  ),
+                                  child: Text(
+                                    '13+',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            SizedBox(
+                              width: 70.0,
+                              height: 20.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.shade800,
+                                    boxShadow: [
+                                      BoxShadow(color: Colors.black12, offset: Offset(3.0, 6.0), blurRadius: 10.0),
+                                    ],
+                                  ),
+                                  child: Text(
+                                    'ratings',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            SizedBox(
+                              width: 70.0,
+                              height: 20.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.shade800,
+                                    boxShadow: [
+                                      BoxShadow(color: Colors.black12, offset: Offset(3.0, 6.0), blurRadius: 10.0),
+                                    ],
+                                  ),
+                                  child: Text(
+                                    'duration',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
                             ),
                           ],
                         ),
                       ],
-                    )
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                            width: 80.0,
+                            height: 100.0,
+                            padding: const EdgeInsets.all(5.0),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                              color: Colors.black,
+                            ),
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.circular(25),
+                                child: Image.asset(
+                                  'images/gps.png',
+                                  fit: BoxFit.fitWidth,
+                                  width: double.infinity,
+                                ))
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
